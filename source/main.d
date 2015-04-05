2d865017beab5378d7815208f78a4a8eef6a5a60
plol <pelle.mansson@gmail.com>
2015-04-05 02:08:51 +0200

    Fix bug, add texture, more!
    Thanks to Luben I got a lot of work done. Woo!
    
    Now there is a cube with a box on one side.

--- source/main.d
+++ source/main.d
@@ -7,6 +7,7 @@ import deimos.glfw.glfw3;
 import derelict.opengl3.gl3;
 import gl3n.linalg;
 
+import imageformats;
 
 string vertex_source = q{
 
@@ -14,11 +15,16 @@ string vertex_source = q{
 
     in vec3 pos;
     uniform mat4 mvp;
+    out vec3 wat;
 
+    in vec2 UV_in;
+    out vec2 UV;
 
     void main() {
         vec4 v = vec4(pos, 1);
         gl_Position = mvp * v;
+        wat = (pos + vec3(1,1,1)) / 2;
+        UV = UV_in;
     }
 };
 
@@ -28,12 +34,17 @@ string fragment_source = q{
     #version 330 core
 
     out vec3 color;
+    in vec3 wat;
+    in vec2 UV;
+
+    uniform sampler2D myTextureSampler;
 
     void main() {
-        color = vec3(0, 1, 1);
+        color = texture2D(myTextureSampler, wat.xy).rgb;
     }
 };
 
+__gshared float max_anisotropic;
 
 GLFWwindow* awkward_init() {
 
@@ -60,9 +71,19 @@ GLFWwindow* awkward_init() {
 
     DerelictGL3.reload();
 
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max_anisotropic);
+
+    // sorcery:
+    uint vertex_array_id;
+    glGenVertexArrays(1, &vertex_array_id);
+    glBindVertexArray(vertex_array_id);
+    // sorcery over
+
     return window;
 }
-
 
 void check_stuff(alias get, alias get_info_log, uint status_param)(uint id) {
     int successful = 0;
@@ -110,6 +131,69 @@ void check_error(int line=__LINE__)() {
     enforce(error == GL_NO_ERROR, "error before line %s: 0x%x".format(line,  error));
 }
 
+
+uint load_texture_from_file(string filename) {
+
+    auto im = read_image(filename);
+
+    uint texture_id;
+    glGenTextures(1, &texture_id);
+
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, cast(int)GL_RGB8, to!int(im.w), to!int(im.h),
+            0, GL_RGB, GL_UNSIGNED_BYTE, im.pixels.ptr);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+            GL_LINEAR_MIPMAP_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_anisotropic);
+
+    return texture_id;
+}
+
+uint generate_buffer_with_data(const float[] data) {
+    uint buffer_id;
+    glGenBuffers(1, &buffer_id);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+    glBufferData(GL_ARRAY_BUFFER, float.sizeof * data.length, data.ptr,
+            GL_STATIC_DRAW);
+    return buffer_id;
+}
+
+
+void bind_buffer_to_vertex_attrib_thing(uint buffer_id, int num_per_thing, uint vertex_attrib_thing) {
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+    glVertexAttribPointer(vertex_attrib_thing, num_per_thing, GL_FLOAT, GL_FALSE, 0, null);
+}
+
+void bind_texture_to_port_thing(uint texture_id, uint port) {
+    glActiveTexture(GL_TEXTURE0 + port);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+}
+
+struct Uniform(T) {
+    uint id;
+    void upload(T val) {
+        static if (is(T == mat4)) {
+            glUniformMatrix4fv(id, 1, true, val.value_ptr);
+        } else static if (is(T == int)) {
+            glUniform1i(id, val);
+        }
+    }
+}
+
+
+Uniform!T create_uniform(T)(uint program_id, string name) {
+    uint id = glGetUniformLocation(program_id, name.toStringz);
+    return Uniform!T(id);
+}
+
+
+
+
 void main()
 {
     scope (exit) {
@@ -118,82 +202,123 @@ void main()
 
     auto window = awkward_init();
 
-    uint vertex_array_id;
-    glGenVertexArrays(1, &vertex_array_id);
-    glBindVertexArray(vertex_array_id);
 
     float[] vertex_buffer_data = [
-    -1.0f, -1.0f, -1.0f, // triangle 1 : begin
-    -1.0f, -1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f, // triangle 1 : end
-    1.0f, 1.0f, -1.0f, // triangle 2 : begin
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, 1.0f, -1.0f, // triangle 2 : end
-    1.0f, -1.0f, 1.0f,
-    -1.0f, -1.0f, -1.0f,
-    1.0f, -1.0f, -1.0f,
-    1.0f, 1.0f, -1.0f,
-    1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, -1.0f,
-    1.0f, -1.0f, 1.0f,
-    -1.0f, -1.0f, 1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f, -1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, -1.0f, -1.0f,
-    1.0f, 1.0f, -1.0f,
-    1.0f, -1.0f, -1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, -1.0f,
-    -1.0f, 1.0f, -1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, -1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f
-             ];
+        -1.0f, -1.0f, -1.0f, // triangle 1 : begin
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f, // triangle 1 : end
+        1.0f, 1.0f, -1.0f, // triangle 2 : begin
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f, // triangle 2 : end
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f
+            ];
+    float[] g_uv_buffer_data = [
+        0.000059f, 1.0f-0.000004f,
+        0.000103f, 1.0f-0.336048f,
+        0.335973f, 1.0f-0.335903f,
+        1.000023f, 1.0f-0.000013f,
+        0.667979f, 1.0f-0.335851f,
+        0.999958f, 1.0f-0.336064f,
+        0.667979f, 1.0f-0.335851f,
+        0.336024f, 1.0f-0.671877f,
+        0.667969f, 1.0f-0.671889f,
+        1.000023f, 1.0f-0.000013f,
+        0.668104f, 1.0f-0.000013f,
+        0.667979f, 1.0f-0.335851f,
+        0.000059f, 1.0f-0.000004f,
+        0.335973f, 1.0f-0.335903f,
+        0.336098f, 1.0f-0.000071f,
+        0.667979f, 1.0f-0.335851f,
+        0.335973f, 1.0f-0.335903f,
+        0.336024f, 1.0f-0.671877f,
+        1.000004f, 1.0f-0.671847f,
+        0.999958f, 1.0f-0.336064f,
+        0.667979f, 1.0f-0.335851f,
+        0.668104f, 1.0f-0.000013f,
+        0.335973f, 1.0f-0.335903f,
+        0.667979f, 1.0f-0.335851f,
+        0.335973f, 1.0f-0.335903f,
+        0.668104f, 1.0f-0.000013f,
+        0.336098f, 1.0f-0.000071f,
+        0.000103f, 1.0f-0.336048f,
+        0.000004f, 1.0f-0.671870f,
+        0.336024f, 1.0f-0.671877f,
+        0.000103f, 1.0f-0.336048f,
+        0.336024f, 1.0f-0.671877f,
+        0.335973f, 1.0f-0.335903f,
+        0.667969f, 1.0f-0.671889f,
+        1.000004f, 1.0f-0.671847f,
+        0.667979f, 1.0f-0.335851f
+            ];
 
-    uint vertex_buffer_id;
+    uint vertex_buffer_id = generate_buffer_with_data(vertex_buffer_data);
+    uint g_uv_buffer_id = generate_buffer_with_data(g_uv_buffer_data);
 
-    glGenBuffers(1, &vertex_buffer_id);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id);
-    glBufferData(GL_ARRAY_BUFFER,
-            float.sizeof * vertex_buffer_data.length,
-            vertex_buffer_data.ptr,
-            GL_STATIC_DRAW);
-
-    auto projection = mat4.perspective(1024, 768, 90, 0.01, 100);
-    auto view = mat4.look_at(vec3(4, 3, 3), vec3(0, 0, 0), vec3(0, 1, 0));
+    auto projection = mat4.perspective(1024, 768, 90, 0.1, 100);
+    auto view = mat4.look_at(vec3(3, 2, 2), vec3(0, 0, 0), vec3(0, 1, 0));
     auto model = mat4.identity;
     auto mvp = projection * view * model;
 
     auto program_id = load_shaders(vertex_source, fragment_source);
 
-    uint mvp_id = glGetUniformLocation(program_id, "mvp");
+    auto mvp_uniform = create_uniform!mat4(program_id, "mvp");
+    auto sampler_uniform = create_uniform!int(program_id, "myTextureSampler");
+
+    uint texture_id = load_texture_from_file("box-01.jpg");
 
     while (!glfwWindowShouldClose(window)) {
-        glEnableVertexAttribArray(0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, null);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        bind_buffer_to_vertex_attrib_thing(vertex_buffer_id, 3, 0);
+        bind_buffer_to_vertex_attrib_thing(g_uv_buffer_id, 2, 1);
+
+        bind_texture_to_port_thing(texture_id, 0);
 
         glUseProgram(program_id);
-        glUniformMatrix4fv(mvp_id, 1, false, mvp.value_ptr);
+
+        mvp_uniform.upload(mvp);
+        sampler_uniform.upload(0);
+
         glDrawArrays(GL_TRIANGLES, 0, to!int(vertex_buffer_data.length / 3));
 
+        glDisableVertexAttribArray(1);
         glDisableVertexAttribArray(0);
-
 
         glfwSwapBuffers(window);
         glfwPollEvents();
 
     }
 }
+
